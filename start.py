7fc7d7f75a7a5801b4dfd875d59dc461c01fc764
gyermolenko <gyermolenko@gmail.com>
2015-08-31 16:08:39 +0300

    Big changes. Now goals are actually processed and it is possible to win.

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from catch_key import catch_key
 from lib import controls
-# from lib import tiles
+from lib import obj_color
 from lib import empty, worker, box, wall, goal, goal_boxed
 
 
@@ -8,7 +8,8 @@ objects = [empty, worker, box, wall, goal, goal_boxed]
 area = []
 area_width, area_height = 0, 0
 worker_x, worker_y = 0, 0
-goal_x, goal_y = 0, 0
+# goal_x, goal_y = 0, 0
+set_of_goals = set()
 
 filename = 'map.txt'
 delimiter = '.'
@@ -18,17 +19,15 @@ def read_map(filename):
     global area
     global area_width, area_height
     global worker_x, worker_y
-    global goal_x, goal_y
+    global set_of_goals
 
     with open(filename) as f:
-        area = [line.strip('\n.').split(delimiter) for line in f]
+        area = [line.strip('\r\n.').split(delimiter) for line in f]
 
     for j, line in enumerate(area):
         for i, ch in enumerate(line):
-            # if ch == goal.file_view:
-            #     goal_x = i
-            #     goal_y = j
-            #     print goal_x, goal_y
+            if ch == goal.file_view or ch == goal_boxed.file_view:
+                set_of_goals.add((i, j))
             for obj in objects:
                 if ch == obj.file_view:
                     line[i] = obj
@@ -41,7 +40,7 @@ def draw():
     for line in area:
         frmt_str = ''
         for obj in line:
-            frmt_str += obj.tile + obj.terminal_view
+            frmt_str += obj.tile + obj.color + obj.terminal_view
         print frmt_str
 
 
@@ -53,71 +52,94 @@ def move(x, y):
                 break
     new_wx, new_wy = wx + x, wy + y
 
-    if not 0 < new_wx < area_width:
-        return
-    if not 0 < new_wy < area_height:
-        return
+    # if not 0 < new_wx < area_width:
+    #     return
+    # if not 0 < new_wy < area_height:
+    #     return
     if area[new_wy][new_wx] == wall:
         return
 
-    if area[new_wy][new_wx] == box:
+    if area[new_wy][new_wx] == box or area[new_wy][new_wx] == goal_boxed:
         if area[new_wy+y][new_wx+x] == wall:
             return
         if area[new_wy+y][new_wx+x] == box:
             return
-        # if area[wy][new_wx+x] == goal:
-        #     area[wy][wx] = empty
-        #     area[wy][new_wx] = worker
-        #     area[wy][new_wx+x] = goal_boxed
-        #     return
+        if area[new_wy+y][new_wx+x] == goal_boxed:
+            return
 
-        area[wy][wx] = empty
-        area[new_wy][new_wx] = worker
-        area[new_wy+y][new_wx+x] = box
+        if (wx, wy) in set_of_goals:
+            area[wy][wx] = goal
+        else:
+            area[wy][wx] = empty
+
+        if area[new_wy][new_wx] == goal_boxed:
+            worker.color = obj_color['BGreen']
+            area[new_wy][new_wx] = worker
+        else:
+            worker.color = obj_color['BWhite']
+            area[new_wy][new_wx] = worker
+
+        if area[new_wy+y][new_wx+x] == goal:
+            area[new_wy+y][new_wx+x] = goal_boxed
+        else:
+            area[new_wy+y][new_wx+x] = box
         return
 
-    # if area[wy][new_wx] == goal_boxed:
-    #     if area[wy][new_wx+x] == wall:
-    #         return
-    #     if area[wy][new_wx+x] == box:
-    #         return
+    if area[new_wy][new_wx] == goal:
+        worker.color = obj_color['BGreen']
+        area[new_wy][new_wx] = worker
+        area[wy][wx] = empty
+        return
 
-    #     area[wy][wx] = empty
-    #     area[wy][new_wx] = worker
-    #     area[wy][new_wx+x] = box
-    #     return
-
-    area[wy][wx] = empty
+    if (wx, wy) in set_of_goals:
+        area[wy][wx] = goal
+    else:
+        area[wy][wx] = empty
+    worker.color = obj_color['BWhite']
     area[new_wy][new_wx] = worker
 
 
+def i_win():
+    for goal_ in set_of_goals:
+        if area[goal_[1]][goal_[0]] is not goal_boxed:
+            return False
+
+    print 'Congratulations!'
+    return True
+
+
 def main():
-    print("\033c")  # clear screen
+    # clear screen. Alternatives: os.system('clear') ## os.system('tput reset')
+    print("\033c")
     read_map(filename)
     draw()
 
-    while True:
+    while not i_win():
         ch = catch_key()
         o = ord(ch)
-        print("\033c")  # Equipotential alternatives: os.system('clear') ## os.system('tput reset')
+        # print ch, o
 
         if o in controls:
             if controls[o] == 'q':
                 print 'quit'
                 break
             elif controls[o] == 'left':
+                print("\033c")
                 move(-1, 0)
                 worker.terminal_view = ' < '
                 draw()
             elif controls[o] == 'right':
+                print("\033c")
                 move(1, 0)
                 worker.terminal_view = ' > '
                 draw()
             elif controls[o] == 'up':
+                print("\033c")
                 move(0, -1)
                 worker.terminal_view = ' ^ '
                 draw()
             elif controls[o] == 'down':
+                print("\033c")
                 move(0, 1)
                 worker.terminal_view = ' V '
                 draw()
@@ -127,6 +149,12 @@ if __name__ == '__main__':
     main()
 
 # v - now worker changes appearance depending on direction of the last move
-# TODO: add at least one goal. Make it not erasable
+# v - added hjkl controls
+# v - goal shoud not disapper when stepped over or boxed
+# v - make worker green when over the goal
+# v - set of goals inst of one goal
+# v - add winning game conditions
+# v - fixed issue with \r inst of \n
+# - choose level
 # TODO: get rid of globals
 # TODO: 'r' should restart game
